aa8f3fb6b7f2054206a5179b20232ab01f31a0ae
nguyendangtaan <dangtan236@gmail.com>
2025-04-30 17:35:09 +0700

    UserScreen UI and addressDialog,LogoutDialog

--- lib/Screeen/user.dart
+++ lib/Screeen/user.dart
@@ -1,11 +1,256 @@
-  import 'package:flutter/material.dart';
-  class UserScreen extends StatelessWidget {
-    const UserScreen({super.key});
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:provider/provider.dart';
+import '../Widget/text_widget.dart';
+import '../provider/dark_theme_provider.dart';
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        body: Text ("User"),
-      );
-    }
+class UserScreen extends StatefulWidget {
+  const UserScreen({Key? key}) : super(key: key);
+
+  @override
+  State<UserScreen> createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+  final TextEditingController _addressTextcontroller = TextEditingController(
+    text: "",
+  );
+
+  @override
+  void dispose() {
+    _addressTextcontroller.dispose();
+    super.dispose();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    final themeState = Provider.of<DarkThemeProvider>(context);
+    final Color color = themeState.getDarkTheme ? Colors.white : Colors.black;
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 15),
+                RichText(
+                  text: TextSpan(
+                    text: 'Hi,  ',
+                    style: const TextStyle(
+                      color: Colors.cyan,
+                      fontSize: 27,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'MyName',
+                        style: TextStyle(
+                          color: color,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        recognizer:
+                            TapGestureRecognizer()
+                              ..onTap = () {
+                                print('My name is pressed');
+                              },
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 5),
+                TextWidget(
+                  text: 'Email@email.com',
+                  color: color,
+                  textSize: 18,
+                  // isTitle: true,
+                ),
+                const SizedBox(height: 20),
+                const Divider(thickness: 2),
+                const SizedBox(height: 20),
+                _listTiles(
+                  title: 'Address 2',
+                  subtitle: 'My subtitle',
+                  icon: IconlyLight.profile,
+                  onPressed: () async {
+                    await _showAddressDialog();
+                  },
+                  color: color,
+                ),
+                _listTiles(
+                  title: 'Orders',
+                  icon: IconlyLight.bag,
+                  onPressed: () {},
+                  color: color,
+                ),
+                _listTiles(
+                  title: 'Wishlist',
+                  icon: IconlyLight.heart,
+                  onPressed: () {},
+                  color: color,
+                ),
+                _listTiles(
+                  title: 'Viewed',
+                  icon: IconlyLight.show,
+                  onPressed: () {},
+                  color: color,
+                ),
+                _listTiles(
+                  title: 'Forget password',
+                  icon: IconlyLight.unlock,
+                  onPressed: () {},
+                  color: color,
+                ),
+                SwitchListTile(
+                  title: TextWidget(
+                    text: themeState.getDarkTheme ? 'Dark mode' : 'Light mode',
+                    color: color,
+                    textSize: 18,
+                    // isTitle: true,
+                  ),
+                  secondary: Icon(
+                    themeState.getDarkTheme
+                        ? Icons.dark_mode_outlined
+                        : Icons.light_mode_outlined,
+                  ),
+                  onChanged: (bool value) {
+                    setState(() {
+                      themeState.setDarkTheme = value;
+                    });
+                  },
+                  value: themeState.getDarkTheme,
+                ),
+                _listTiles(
+                  title: 'Logout',
+                  icon: IconlyLight.logout,
+                  onPressed: () {
+                    _showLogoutDialog();
+                  },
+                  color: color,
+                ),
+                listTileAsRow(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _showLogoutDialog() async {
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Row(
+            children: [
+              Image.asset(
+                'assets/images/warning-sign.png',
+                height: 20,
+                width: 20,
+                fit: BoxFit.fill,
+              ),
+              const SizedBox(
+                  width: 8
+              ),
+              const Text('Sign out'),
+            ],
+          ),
+          content: const Text('Do you want to sign out?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                if (Navigator.canPop(context)) {
+                  Navigator.pop(context);
+                }
+              },
+              child: TextWidget(
+                text: 'Cancel',
+                color: Colors.cyan,
+                textSize: 18,
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: TextWidget(
+                  text: 'OK',
+                  color: Colors.red,
+                  textSize: 18
+              ),
+            ),
+          ],
+        );
+      }
+    );
+  }
+
+  Future<void> _showAddressDialog() async {
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Update'),
+          content: TextField(
+            // onChanged: (value){
+            //   print('_addressTextcontroller.text ${_addressTextcontroller.text}');
+            // },
+            controller: _addressTextcontroller,
+            maxLines: 5,
+            decoration: const InputDecoration(hintText: 'Your Address'),
+          ),
+          actions: [TextButton(onPressed: () {}, child: const Text('Update'))],
+        );
+      },
+    );
+  }
+
+  Widget _listTiles({
+    required String title,
+    String? subtitle,
+    required IconData icon,
+    required Function onPressed,
+    required Color color,
+  }) {
+    return ListTile(
+      title: TextWidget(
+        text: title,
+        color: color,
+        textSize: 22,
+        // isTitle: true,
+      ),
+      subtitle: TextWidget(
+        text: subtitle == null ? "" : subtitle,
+        color: color,
+        textSize: 18,
+      ),
+      leading: Icon(icon),
+      trailing: const Icon(IconlyLight.arrowRight2),
+      onTap: () {
+        onPressed();
+      },
+    );
+  }
+
+  // Alternative code for the listTile.
+  Widget listTileAsRow() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: <Widget>[
+          const Icon(Icons.settings),
+          const SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [Text('Title'), Text('Subtitle')],
+          ),
+          const Spacer(),
+          const Icon(Icons.chevron_right),
+        ],
+      ),
+    );
+  }
+}
